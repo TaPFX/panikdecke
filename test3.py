f5a28c8728ddb9b5923a073066ab489d2a5e487c
Florian B <flo@tap>
2024-01-28 14:46:40 +0100

    add

---
+++ test3.py
@@ -0,0 +1,34 @@
+
+import time
+import asyncio
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+async def main(frequency):
+    time_last = time.time()
+    x = 0
+    x_end = 3
+    sleep_meas = 0
+    await asyncio.sleep(1 / (frequency))
+    await asyncio.sleep(1 / (frequency))
+    await asyncio.sleep(1 / (frequency))
+    sleep_meas = time.time() - time_last
+    print(f"{frequency}: {1/(sleep_meas/x_end)}")
+    return 1/(sleep_meas/x_end)
+
+f = np.geomspace(10, 500, 50)
+
+#f = [1, 10, 200, 300]
+
+f_meas = []
+for freq in f:
+    f_meas.append(asyncio.run(main(freq)))
+
+fig, ax = plt.subplots()
+ax.plot(f, f_meas)
+ax.set_xlabel("commanded frequency")
+ax.set_ylabel("measured frequency")
+
+plt.savefig("test3.png")
+plt.show()
